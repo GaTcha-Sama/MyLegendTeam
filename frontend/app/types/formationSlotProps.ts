92af1b38508cfa9b44ff677efed1c4f19d10cc75
Jeremy Louet <jeremy.louet29@hotmail.fr>
2025-08-09 14:25:00 +0200

    Refacto FormationSlot

---
+++ frontend/app/types/formationSlotProps.ts
@@ -0,0 +1,13 @@
+import { Player as PlayerType } from "./players";
+import { Theme, Sport } from "./sports";
+
+export interface FormationSlotProps {
+  position: string;
+  player: PlayerType | null;
+  onDropPlayer: (position: string, player: PlayerType | null) => void;
+  isPlayerAlreadyPlaced: boolean;
+  theme: Theme;
+  positionId: string;
+  sport: Sport;
+  draggedPlayer?: PlayerType | null; // Nouveau prop pour le joueur en cours de drag
+}
