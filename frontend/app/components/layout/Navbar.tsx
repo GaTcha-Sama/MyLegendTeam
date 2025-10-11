9a37d9d1878edf4e6d3777519493243bc6a5a135
Jeremy Louet <jeremy.louet29@hotmail.fr>
2025-10-11 15:04:41 +0200

    Merge login-google to dev

--- frontend/app/components/layout/Navbar.tsx
+++ frontend/app/components/layout/Navbar.tsx
@@ -68,7 +68,7 @@ export const Navbar = () => {
               <>
                 <span className="text-yellow-500 flex flex-col items-center gap-1 border-2 border-yellow-500 rounded-3xl px-6 py-3 font-[family-name:var(--font-title)]">
                 <div className="flex items-center gap-2">
-                  Welcome<span className="text-blue-500 font-bold">{username}</span>
+                  Welcome <span className="text-blue-500 font-bold">{username}</span>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-blue-500">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
                   </svg>
