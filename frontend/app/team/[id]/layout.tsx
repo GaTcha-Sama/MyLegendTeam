1c009ed270a53aa11d6d6237374c93328ae88374
Jeremy Louet <jeremy.louet29@hotmail.fr>
2025-11-28 19:27:58 +0100

    Modify layout

--- frontend/app/team/[id]/layout.tsx
+++ frontend/app/team/[id]/layout.tsx
@@ -3,7 +3,6 @@ export default function TeamLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // Layout spécifique pour la page de partage - sans Navbar ni Footer
   return (
     <>
       {children}
